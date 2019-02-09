0dbe7c571c8a218281e827b1c6bd374ddc1c2e12
Vinicius <vinicius.2010.s@gmail.com>
2019-02-09 17:56:00 -0200

    Acess token

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'src'));
 app.set('view engine', 'ejs');
 
 const baseURL = 'https://api.github.com'
-const accessToken = '?access_token=7ad432e1e287fa0f675c747b03293db146e2aa17'
+const accessToken = '?access_token=02a1685d15f35d64e8d8043792133f95a91d38a3'
 
 app.get('/', (req, res) => {
     res.render('index', { data: null });
@@ -25,7 +25,7 @@ app.get('/users', async (req, res) => {
     const repos = req.query.sort ?
         await axios.get(`https://api.github.com/users/${user}/repos`, {
             params: {
-                accessToken: '7ad432e1e287fa0f675c747b03293db146e2aa17',
+                access_token: '02a1685d15f35d64e8d8043792133f95a91d38a3',
                 sort: req.query.sort
             }
         })
@@ -49,7 +49,7 @@ app.get('/repos', async (req, res) => {
 
     await axios.get(`${baseURL}/repos/${repoName}${accessToken}`)
         .then(result => {
-            return res.render('repo', { data: result.data });
+            return res.render('repositories', { data: result.data });
         })
         .catch(err => {
             console.log('Status: ', err.response.status)
